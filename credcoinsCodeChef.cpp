639ffa5810a8a19055609f9bf45ca9fa142ed152
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-12-09 20:46:18 +0530

    Add files via upload

---
+++ credcoinsCodeChef.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int x=(a*b)/100;
+	    cout<<x<<endl;
+	}
+	return 0;
+}
